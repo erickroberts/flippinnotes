2bfd1ea065ea1b71c80b5e0e863d2d684513e56e
elrdevelopment <elrdevllc@gmail.com>
2020-02-01 17:06:29 -0600

    update tailwind and cloudinary integrated

---
+++ src/components/Menu.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="headerMenu" class >
+        <ul class="flex">
+            <li>
+                <cld-image publicId="flippin-icon" secure="true">
+                    <cld-transformation width="150" height="150" radius="20" />
+git stat
+                </cld-image>
+            </li>
+            <li class="mr-6">
+                <a class="text-blue-500 hover:text-blue-800" href="#">Active</a>
+            </li>
+            <li class="mr-6">
+                <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+            </li>
+            <li class="mr-6">
+                <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+            </li>
+            <li class="mr-6">
+                <a class="text-blue-500 hover:text-blue-800d" href="#">Disabled</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Menu"
+    }
+</script>
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {},
+    fontFamily: {
+          'sans': ['freight-sans-pro', 'sans-serif'],
+         'serif': ['freight-text-pro', 'serif'],
+         'mono': ['interstate-mono, monospace;', 'Menlo'],
+         'display': ['meno-display', 'serif','Oswald'],
+         'body': ['freight-sans-pro', 'sans-serif'],
+}
+  },
+  variants: {},
+  plugins: []
+}
